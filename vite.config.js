16b4a29638099953b09cb00b7ed49ce7957e45ed
iDamjan <damjan.stojanovski123@gmail.com>
2024-04-12 12:58:17 +0200

    update name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import glsl from "vite-plugin-glsl";
 export default {
   root: "src/",
   publicDir: "../static/",
-  base: "/3DGym",
+  base: "/3DGym/",
   server: {
     host: true, // Open to local network and display URL
     open: !("SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env), // Open if it's not a CodeSandbox
